5cf702cec9c78bedd723ed2f2d8320890d818f2d
AntunesD <antunesd90@gmail.com>
2024-12-02 19:27:41 +0100

    début app test jest

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
+import { of } from 'rxjs'; // Pour retourner un Observable simulé
 
 import { AppComponent } from './app.component';
-
+import { SessionService } from './services/session.service';
 
 describe('AppComponent', () => {
+  let sessionService: SessionService;
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -18,12 +23,30 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        // Mock du SessionService
+        { provide: SessionService, useValue: { $isLogged: jest.fn() } }
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    sessionService = TestBed.inject(SessionService);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should return observable from $isLogged()', () => {
+    // Simuler le retour d'un Observable
+    const mockObservable = of(true);
+    jest.spyOn(sessionService, '$isLogged').mockReturnValue(mockObservable);
+
+    // Appeler la méthode $isLogged et vérifier qu'elle retourne bien un observable
+    component.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(true);  // Vérifier que la valeur retournée est bien `true`
+    });
   });
 });
